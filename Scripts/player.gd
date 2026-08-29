b90eb4be02a457886c6cc0e65f27f393c500c410
Jamile Rendon-Torres <shadowmoonhuntress@gmail.com>
2026-08-28 22:13:18 -0500

    Uploaded Godot Engine Project
    I uploaded the new Godot Engine project file to continue creating my game. I was advised to use this engine specifically for 2D pixel art as it would be way easier than dealing with Unreal Engine.

---
+++ Scripts/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+var direction : Vector2 = Vector2()
+
+func read_input():
+	velocity = Vector2()
+	direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+		direction = Vector2(0, -1)
+		
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1
+		direction = Vector2(0, 1)
+		
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+		direction = Vector2(-1, 0)
+		
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+		direction = Vector2 (1, 0)
+		
+	direction = direction.normalized()
+	velocity = direction * 200 #sets the built-in CharacterBody2D velocity
+	
+func _physics_process(delta):
+	read_input()
+	move_and_slide() #allows the character to move using the self velocity.
