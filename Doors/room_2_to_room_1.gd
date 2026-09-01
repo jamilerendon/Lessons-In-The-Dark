f9f9d5837acd00477c59e3e21fa3166c8ee24af7
zXxKanjixXz <steven.mai599@gmail.com>
2026-09-01 02:31:34 -0500

    Main Menu
    Started on creating main menu, "Play" and "Quit Game" work but "Settings" has not been worked on.

--- Doors/room_2_to_room_1.gd
+++ Doors/room_2_to_room_1.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body):
 	print("Object entered: ", body.name)
 	if body.is_in_group("player"):
 		print("Player confirmed, teleporting...")
-		TransitionManager.go_to_room("res://Scene/Main.tscn", "EntryFromRoom2")
+		Transition.go_to_room("res://Scene/Main.tscn", "EntryFromRoom2")
