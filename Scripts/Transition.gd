c290fda0e31546599f193823f562cda4a886f0ef
zXxKanjixXz <steven.mai599@gmail.com>
2026-09-09 00:40:53 -0500

    PauseMenu not active when invisible
    Fixed the Issue of the PauseMenu not being active when it wasn't visible in game.
    
    Known Issues of the PauseMenu settings and MainMenu settings are not synced together, Back to Menu when you got into settings from PauseMenu shows a black background instead of the game being paused.

--- Scripts/Transition.gd
+++ Scripts/Transition.gd
@@ -2,11 +2,15 @@ extends CanvasLayer
 
 @onready var fade_rect: ColorRect = $ColorRect
 var next_poe: String = "Default" #next_poe means the next point of entry
+var return_scene_path: String = "res://Scene/Menu/MainMenu.tscn"
+var return_entry_point: String = "EntryDefault"
+var reopen_pause_menu: bool = false
 
-func room_change(scene: String, _poe_name: String):
+func room_change(scene: String, poe_name: String):
+	next_poe = poe_name
 	await fade_out()
 	get_tree().call_deferred("change_scene_to_file", scene)
-	await get_tree().process_frame # wait a frame for new scene to load
+	await get_tree().process_frame
 	await fade_in()
 
 func fade_out() -> void:
